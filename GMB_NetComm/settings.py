8bee8711c6a8c538af13bfd0fd4da8a1ac1a87e1
Boris Mendelblat <boris.mand89@gmail.com>
2021-08-17 20:39:08 +0300

    added pathes and cert for amazon

--- GMB_NetComm/settings.py
+++ GMB_NetComm/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'GMB_NetComm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CommunicationLTD',
+        'USER': 'netcomm',
+        'PASSWORD': 'Mf439&0$',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
     }
 }
 
--- net_comm_web/urls.py
+++ net_comm_web/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('changepassword/', views.changepassword, name='change_pass'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
