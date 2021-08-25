79364e54d2cd865a24d666f962b7f7d85e82e8a7
Moshe Gotam <mgotam@gmail.com>
2021-08-25 22:24:34 +0300

    working change pass

--- GMB_NetComm/settings.py
+++ GMB_NetComm/settings.py
@@ -118,12 +118,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'commltdhit@gmail.com'
-EMAIL_HOST_PASS = 'GBM1234!'
+EMAIL_HOST_PASSWORD = 'ywecskiggirsffad'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
